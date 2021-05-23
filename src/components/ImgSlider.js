5ecee16bfa9cbde1d13fbff5e093d864a9271440
Alexis Tercero <alexistercero@Mini-de-Alexis.fibertel.com.ar>
2021-05-23 12:21:32 -0300

    Update img slider

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -5,23 +5,75 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function ImgSlider() {
-    return (
-        <Carousel>
+    let settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+      };
+      return (
+        <Carousel {...settings}>
             <Wrap>
-                <img src="/images/slider-badging.jpg" />
+                <a>
+                <img src="/images/slider-badging.jpg" alt="" />
+                </a>
             </Wrap>
+
             <Wrap>
-                <img src="/images/slider-badging.jpg" />
+                <a>
+                <img src="/images/slider-scale.jpg" alt="" />
+                </a>
             </Wrap>
-        </Carousel>
+
+            <Wrap>
+                <a>
+                <img src="/images/slider-badag.jpg" alt="" />
+                </a>
+            </Wrap>
+
+            <Wrap>
+                <a>
+                <img src="/images/slider-scales.jpg" alt="" />
+                </a>
+            </Wrap>
+    </Carousel>
     )
 }
 
 export default ImgSlider
 
 const Carousel = styled(Slider)` 
+    margin-top: 20px;
+
+    .slick-list {
+        overflow: visible;
+    }
 
 `
 const Wrap = styled.div`
+    border-radius: 4px;
+    cursor: pointer;
+    position: relative;
+
+
+
+    img {
+        border: 4px solid transparent;
+        border-radius: 4px;
+        width: 100%;
+        height: 100%;
+        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+
+
+        &:hover {
+        padding: 0;
+        border: 4px solid rgba(249, 249, 249, 0.8);
+        transition-duration: 300ms;
+    }
+
+}
 
 `
